013d1f82da3b8045756218decf478f5561ca4a92
La Vinia Parker <laviniaparker@La-Vinias-MacBook-Pro.local>
2023-11-02 16:28:18 -0400

    updated css for clock

---
+++ script.js
@@ -0,0 +1,12 @@
+const hourEl = document.querySelector('.hour'); 
+const minuteEl = document.querySelector('.minute'); 
+const secondEl = document.querySelector('.second'); 
+const timeEl = document.querySelector('.time'); 
+const dateEl = document.querySelector('.date'); 
+const toggleEl = document.querySelector('.toggle'); 
+
+
+
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
